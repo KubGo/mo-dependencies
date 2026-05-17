d4c01364868d529fefd37286144dd7b9591a266e
kubgo <jakub.golcgdy@gmail.com>
2026-05-17 15:07:59 +0200

    Add Javadocs to DependencyTreeResolver

--- src/main/java/dependencies/DependencyTreeResolver.java
+++ src/main/java/dependencies/DependencyTreeResolver.java
@@ -13,6 +13,9 @@ import modelica.pathresolvers.FileStructurePathResolver;
 import java.io.IOException;
 import java.util.*;
 
+/**
+ * Resolves dependency tree for the library
+ */
 public class DependencyTreeResolver {
 	ModelicaFilesStructure filesStructure = new ModelicaFilesStructure();
 	ModelicaFileReader modelicaFileReader = new ModelicaFileReader();
@@ -23,6 +26,9 @@ public class DependencyTreeResolver {
 	private String libraryPath;
 	private final List<IFilter> filters = new ArrayList<>();
 
+	/**
+	 * @param filters filters to use on dependencies of each class
+	 */
 	public DependencyTreeResolver(IFilter... filters) {
 		Arrays.stream(filters).forEach(this::addFilter);
 	}
@@ -39,10 +45,18 @@ public class DependencyTreeResolver {
 		filters.add(filter);
 	}
 
+	/**
+	 * @param dependencies dependency tree of other library that might influence library
+	 *                     dependencies.
+	 */
 	public void addLibraryDependencies(Map<String, ? extends IClassDependencies> dependencies) {
 		parentDependenciesResolver.addTreeForParentSearching(dependencies);
 	}
 
+	/**
+	 * @param path path to library top package
+	 * @param libraryName library name
+	 */
 	public void generateLibraryDependencies(
 			String path,
 			String libraryName){
@@ -56,17 +70,8 @@ public class DependencyTreeResolver {
 					List<ClassInfo> classInfoList = packageInfo.getClassDefinitions();
 					classInfoList.forEach(classInfo -> {
 						fileStructurePathResolver.setCurrentPackage(packageInfo.getModelicaPath());
-						String modelicaPath = classInfo.getModelicaPath();
-						String className = classInfo.getClassName();
 						try {
-							ClassDependenciesResolver classDependenciesResolver = new ClassDependenciesResolver(
-									className,
-									modelicaFileReader.readFile(
-											classInfo.path));
-							classDependenciesResolver.resolveInternalDependencies();
-							classDependenciesResolver.resolveLibraryDependencies(fileStructurePathResolver);
-							classDependenciesResolver.filter(filters);
-							dependencyTree.put(modelicaPath, classDependenciesResolver);
+							resolveClassDependencies(classInfo);
 						} catch (IOException e) {
 							throw new RuntimeException(e);
 						}
@@ -76,11 +81,36 @@ public class DependencyTreeResolver {
 		dependencyTree = parentDependenciesResolver.resolveParentDependencies(dependencyTree);
 	}
 
+	/**
+	 * @param classInfo Class info for resolving the class dependencies
+	 *
+	 * @throws IOException when class cannot be read
+	 */
+	private void resolveClassDependencies(ClassInfo classInfo) throws IOException {
+		ClassDependenciesResolver classDependenciesResolver = new ClassDependenciesResolver(
+				classInfo.getClassName(),
+		                                                                                    modelicaFileReader.readFile(
+																									classInfo.path));
+		classDependenciesResolver.resolveInternalDependencies();
+		classDependenciesResolver.resolveLibraryDependencies(fileStructurePathResolver);
+		classDependenciesResolver.filter(filters);
+		dependencyTree.put(classInfo.getModelicaPath(), classDependenciesResolver);
+	}
+
+	/**
+	 * Include dependencies from other libraries in this tree.
+	 * To add other dependencies see {@link #addLibraryDependencies(Map) addLibraryDependencies}
+	 */
 	public void includeParentsDependentClasses(){
 		dependencyTree = parentDependenciesResolver.resolveParentDependencies(dependencyTree);
 
 	}
 
+	/**
+	 * @param className name of the class
+	 *
+	 * @return class dependencies for given class. If class name wasn't found null is returned.
+	 */
 	public IClassDependencies getClassDependencies(String className) {
 		if (dependencyTree.containsKey(className)) {
 			return dependencyTree.get(className).toSavableClassDependencies();
@@ -89,6 +119,9 @@ public class DependencyTreeResolver {
 		}
 	}
 
+	/**
+	 * @return Simplified class dependencies that can be serialized and saved to file.
+	 */
 	public TreeMap<String, ClassDependencies> getSimplifiedClassDependencies() {
 		TreeMap<String, ClassDependencies> dependencies = new TreeMap<>();
 		for (String key : dependencyTree.keySet()) {
@@ -97,6 +130,10 @@ public class DependencyTreeResolver {
 		return dependencies;
 	}
 
+	/**
+	 * @param writers Writer classes that extend {@link AbstractDependenciesWriter} and can save
+	 *                {@link ClassDependencies} to file.
+	 */
 	public void saveDependencies(AbstractDependenciesWriter... writers) {
 		for (AbstractDependenciesWriter writer : writers) {
 			writer.setLibraryName(libraryName);
